bfa0b73c3d441b9169b0bad00d0e144676330a3e
Paul Ty <p.ty@logaviv.com>
2017-05-16 14:51:00 +0200

    modification globals var

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -1,2 +1,2 @@
-export const URL_WEB_API_PROD = 'http://clupwprioptest.agrimarket.fr/api'
+export const URL_WEB_API_PROD = 'http://clubwpriopwebapitest.agrimarket.fr/api'
 export const URL_WEB_API_DEV = 'http://localhost:37528/api'
