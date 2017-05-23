dd231be3db99ec0fdf7800ff1d294c830f3380af
Paul Ty <p.ty@logaviv.com>
2017-05-23 17:10:56 +0200

    modification url api

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -1,4 +1,4 @@
-export const URL_WEB_API_PROD = 'http://clubwpriopwebapitest.agrimarket.fr/api'
+export const URL_WEB_API_PROD = 'http://clubapi.logaviv.com/api'
 export const URL_WEB_API_DEV = 'http://localhost:37528/api'
 export const REFRESH_INTERVAL = 3000
 
